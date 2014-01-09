47abd65dcdb936864bbdfa62186019cadff8f870
hejunbiz <hejunbiz@163.com>
2014-01-09 02:59:15 -0800

    创建系统

---
+++ protected/config/main.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * 系统配置
+ * @Author		HeJun(hejun@hejun.biz)
+ * @Link		www.hejun.biz
+ * @license		http://www.hejun.biz/license
+ * @version		v1.0
+ */
+return array(
+	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+);
+?>
---
+++ protected/controllers/SiteController.php
@@ -0,0 +1,7 @@
+<?php
+class SiteController extends CController{
+	public function actionIndex(){
+		echo 'Hello World';
+	}
+}
+?>
