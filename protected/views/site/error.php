4fac4e7b62e99fdef3f8bd879466c3c0e4205654
hejunbiz <hejunbiz@163.com>
2014-01-10 08:29:27 -0800

    添加系统管理首页系统信息显示

--- protected/views/site/error.php
+++ protected/views/site/error.php
@@ -1,4 +1,15 @@
-<h2>Error <?php echo $code; ?></h2>
-<div class = "error">
-	<?php echo CHtml::encode($message); ?>
-</div>
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<link rel="stylesheet" type="text/css" href="/static/admin/css/manage.css">
+		<script type="text/javascript" src="/static/js/jquery/jquery.js"></script>
+		<title>Error <?php echo $code; ?>-后台管理系统</title>
+	</head>
+	<body>
+		<h2>Error <?php echo $code; ?></h2>
+		<div class = "error">
+			<?php echo CHtml::encode($message); ?>
+		</div>
+	</body>
+</html>
